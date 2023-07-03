d202503ebf1ccc3580dce038487e21ca8278407e
bertknl <bertliu@hku.hk>
2023-07-03 11:25:13 +0800

    Added the login screen

--- app/src/main/java/edu/upenn/sas/archaeologyapp/ui/LoginActivity.java
+++ app/src/main/java/edu/upenn/sas/archaeologyapp/ui/LoginActivity.java
@@ -1,26 +1,19 @@
 package edu.upenn.sas.archaeologyapp.ui;
-import android.os.Handler;
+
 import android.os.Bundle;
+import android.os.Handler;
+
 import edu.upenn.sas.archaeologyapp.R;
 import edu.upenn.sas.archaeologyapp.util.Constants;
-/**
- * The splash activity
- * @author Created by eanvith on 24/12/16.
- */
-public class SplashActivity extends BaseActivity
-{
-    /**
-     * App is launched
-     * @param savedInstanceState - state from memory
-     */
+
+public class LoginActivity extends BaseActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        // Make this a fullscreen activity
-        super.requestFullScreenActivity();
+
         // Setting the layout for this activity
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_login);
         // Wait for specified time and start main activity
         new Handler().postDelayed(new Runnable() {
             /**
@@ -31,16 +24,11 @@ public class SplashActivity extends BaseActivity
             {
                 // Here' we change the starting screen from Mainactivity to Login.
                 //Without a proper login token. We do not allow using the application.
-                SplashActivity.super.startActivityUsingIntent(LoginActivity.class);
+                //SplashActivity.super.startActivityUsingIntent(MainActivity.class);
             }
         }, Constants.SPLASH_TIME_OUT);
     }
 
-    /**
-     * Disable on back pressed - this is the splash screen, the user will automatically be taken to the next screen.
-     */
-    @Override
-    public void onBackPressed()
-    {
-    }
+
+
 }
